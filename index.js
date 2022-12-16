c32d0ca4baedb6fb1bd95027c6c5eed53df71fcb
Ambuj Singh <7ambujsingh@gmail.com>
2022-12-16 23:35:20 +0530

    #first

--- index.js
+++ index.js
@@ -6,16 +6,14 @@ const authRoutes = require("./routes/auth.js");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-require("dotenv").config();
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+	res.send("Hello, World!");
 });
 
-app.use('/auth', authRoutes)
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const { sign } = require("crypto");
+const express = require("express");
 
-const {signup , login} = require('../controllers/auth.js');
+const { signup, login } = require("../controllers/auth.js");
 
 const router = express.Router();
 
-router.post('/signup');
-router.post('/login');
+router.post("/signup", signup); // here i was stuck then prashant kumar (BRIQ); fixed it i was missing handler signup and signin
+router.post("/login", login);
 
 module.exports = router;
