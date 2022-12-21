fedffc1a1ef47b0693f5ec23dfb33c2f6e8caf12
Ambuj Singh <7ambujsingh@gmail.com>
2022-12-22 02:39:51 +0530

    #22dec

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
-	res.send("Server is running");
+	res.send("Hello, World!");
 });
 
 app.use("/auth", authRoutes);
